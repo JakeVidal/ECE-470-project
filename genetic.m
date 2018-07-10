0ac307a0f1c613ff10ad0a19417b8a41c6c411ef
JakeVidal <jrvidal@shaw.ca>
2018-07-09 19:11:12 -0700

    basic genetic algorithm structure complete

--- genetic.m
+++ genetic.m
@@ -5,12 +5,12 @@
 % 4. This function breeds the pairs using mutation and crossover
 % 5. This function returns the new population
 
-function [new_chromosomes] = genetic(chromosomes,population)
+function [new_chromosomes,fitness] = genetic(chromosomes,num_features,population,crossover_rate,mutation_rate)
     for i = 1:population
         fitness(i) = sum(chromosomes(:,i));
     end    
 
-    [~,f] = sort(fitness,'descend')
+    [~,f] = sort(fitness,'descend');
     
     v = zeros(population,1);
     for j = 1:population
@@ -19,11 +19,23 @@ function [new_chromosomes] = genetic(chromosomes,population)
     
     genetic_raffle = repelem(f,v);
     
-    first_parent = genetic_raffle(round((size(genetic_raffle,2)-1)*rand(1,1)+1));
-    second_parent = first_parent;
-    while second_parent == first_parent
-        second_parent = genetic_raffle(round((size(genetic_raffle,2)-1)*rand(1,1)+1));
+    for k = 1:population
+        first_parent = genetic_raffle(round((size(genetic_raffle,2)-1)*rand(1,1)+1));
+        second_parent = first_parent;
+        while second_parent == first_parent
+            second_parent = genetic_raffle(round((size(genetic_raffle,2)-1)*rand(1,1)+1));
+        end
+        
+        new_chromosomes(:,k) = chromosomes(:,first_parent);
+        for m = 1:num_features
+            if rand(1,1) >= crossover_rate
+                new_chromosomes(m,k) = chromosomes(m,second_parent);
+            end
+            if rand(1,1) >= mutation_rate
+                new_chromosomes(m,k) = ~new_chromosomes(m,k);
+            end
+        end
     end
-    
+
 end
 
--- testing.m
+++ testing.m
@@ -1,11 +1,16 @@
 % Genetic algorithm testing script
 % 1. This script generates an initial population of solutions
 % 2. This script runs the genetic algorithm on the initial population
-population = 10;
+population = 100;
 num_features = 30;
+mutation_rate = 0.02;
+crossover_rate = 0.5;
 
 for i = 1:population
     chromosomes(:,i) = round(rand(num_features,1));
 end
 
-genetic(chromosomes,population)
+for j = 1:1000
+    [chromosomes,fitness] = genetic(chromosomes,num_features,population,crossover_rate,mutation_rate);
+    sum(fitness)
+end
